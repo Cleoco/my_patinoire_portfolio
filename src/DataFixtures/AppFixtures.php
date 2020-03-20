00cda055eea30d907ee8ff9c1a0476b578d7d54f
Cleoco <cleo.cosnier@gmail.com>
2020-03-20 16:13:44 +0100

    add fixtures on blog

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,14 +2,20 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
+use App\Entity\Filter;
+use App\Entity\Article;
 use App\Entity\FiltersBlog;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create();
+
            $filter2 = new FiltersBlog();
            $filter2->setName('PRINT');
            $filter2->setImage('print-picto-5e6bc58a14613370035551.svg');
@@ -29,6 +35,34 @@ class AppFixtures extends Fixture
            $filter5->setName('DEV');
            $filter5->setImage('Groupe116.svg');
            $manager->persist($filter5);
+
+        for($j = 1; $j < mt_rand(6,15); $j++){
+            $article = new Article();
+            $content = '<p>' . join($faker->paragraphs(5),'</p><p>').'</p>';
+            $article -> setTitle($faker->sentence())
+                     -> setContent($content)
+                     -> setCreatedAt($faker->dateTimeBetween('-6 months'))
+                     -> setLink($faker->url)
+                     -> setSource($faker->url)
+                     -> setUpdatedAt($faker->dateTimeBetween('-6 months'))
+                     -> setFilter($filter2,$filter3,$filter4,$filter5)
+                     
+        }
+
+
+        for($k = 1; $k < mt_rand(4,10); $k++){
+            $comment = new Comment();
+            $content = '<p>' . join($faker->paragraphs(2),'</p><p>') . '</p>';
+
+  
+            $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+            $comment-> setAuthor($faker->name)
+                    -> setContent($content)
+                    -> setCreatedAt($faker->dateTimeBetween("-" .  $days . ' days' ))
+                    -> setArticle($article);
+                    -> setEmail($faker->email);
+
+        }
 
         $manager->flush();
     }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
@@ -78,11 +80,17 @@ class Article
      */
     private $keyWords;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="article", orphanRemoval=true)
+     */
+    private $comments;
+
     public function __construct()
     {
         $now = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
         $this->setCreatedAt($now);
         $this->setUpdatedAt($now);
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -219,6 +227,37 @@ class Article
     public function setKeyWords(?string $keyWords): self
     {
         $this->keyWords = $keyWords;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
 
         return $this;
     }
